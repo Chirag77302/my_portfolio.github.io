66e8fa1d0ebe17250ad8ef37c368caf5e076c8ab
Chirag Jindal <69391451+Chirag77302@users.noreply.github.com>
2021-03-14 12:04:16 +0530

    make it fabulous

--- end.js
+++ end.js
@@ -1,28 +1,28 @@
 
     
     $(function () {
-        $(document).scroll(function () {
-            var $nav = $("#mainnav");
-            var $sec1 = $("#intro")
-            $nav.toggleClass("scrolled", $(this).scrollTop() > $sec1.height());
-            $nav.toggleClass("shadow",$(this).scrollTop() > $sec1.height());
-        });
-    });
+      $(document).scroll(function () {
+          var $nav = $("#mainnav");
+          var $sec1 = $("#intro")
+          $nav.toggleClass("scrolled", $(this).scrollTop() > $sec1.height());
+          $nav.toggleClass("shadow",$(this).scrollTop() > $sec1.height());
+      });
+  });
 
-  
-          mybutton = document.getElementById("myBtn");
 
-          window.onscroll = function() {scrollFunction()};
+        mybutton = document.getElementById("myBtn");
 
-          function scrollFunction() {
-            if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
-              mybutton.style.display = "block";
-            } else {
-              mybutton.style.display = "none";
-            }
+        window.onscroll = function() {scrollFunction()};
+
+        function scrollFunction() {
+          if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+            mybutton.style.display = "block";
+          } else {
+            mybutton.style.display = "none";
           }
+        }
 
-          function topFunction() {
-            document.body.scrollTop = 0;
-            document.documentElement.scrollTop = 0; 
-          }
+        function topFunction() {
+          document.body.scrollTop = 0;
+          document.documentElement.scrollTop = 0; 
+        }
